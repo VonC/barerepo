00da1af8f3be7cb5f78ecc8eecc904d1c1ab0ed2
VonC <vonc@laposte.net>
2022-04-03 23:37:46 +0200

    queue test Sub removal

--- internal/commits/queue_test.go
+++ internal/commits/queue_test.go
@@ -2,38 +2,35 @@ package commits_test
 
 import (
 	"fmt"
-	"path/filepath"
-	"runtime"
-	"strings"
 	"testing"
 	"time"
 
 	"github.com/VonC/barerepo/internal/commits"
-	"github.com/hack-pad/hackpadfs"
 	"github.com/hack-pad/hackpadfs/os"
 )
 
 func TestQueue(t *testing.T) {
 	ofs := os.NewFS()
 	var err error
-
-	_, filename, _, ok := runtime.Caller(0)
-	if !ok {
-		t.Errorf("unable to get the current filename")
-	}
-	dirname := filepath.Dir(filename)
-	var ffs hackpadfs.FS
-	dirname = strings.ReplaceAll(dirname, "c:\\", "c/")
-	dirname = strings.ReplaceAll(dirname, "\\", "/")
-	ffs, err = ofs.Sub(dirname)
-	if err != nil {
-		t.Fatalf("Error on fs.Sub: %+v", err)
-	}
-	ofs, ok = ffs.(*os.FS)
-	if !ok {
-		t.Fatalf("unable to get the current FS")
-	}
-
+	/*
+		_, filename, _, ok := runtime.Caller(0)
+		if !ok {
+			t.Errorf("unable to get the current filename")
+		}
+		dirname := filepath.Dir(filename)
+		var ffs hackpadfs.FS
+		dirname = strings.ReplaceAll(dirname, "c:\\", "c/")
+		dirname = strings.ReplaceAll(dirname, "\\", "/")
+		ffs, err = ofs.Sub(dirname)
+		if err != nil {
+			t.Fatalf("Error on fs.Sub: %+v", err)
+		}
+		ofs, ok = ffs.(*os.FS)
+		if !ok {
+			t.Fatalf("unable to get the current FS")
+		}
+	*/
+	commits.Printf(fmt.Sprintf("test done"))
 	var q commits.Queue
 	if q, err = commits.NewQueue("test", ofs, process); err != nil {
 		t.Fatalf("Error on NewQueue: %+v", err)
