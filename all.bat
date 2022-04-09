ffdd91437e0934f33c2a47d5571b8c740795d358
VonC <vonc@laposte.net>
2022-04-09 22:00:33 +0200

    all.bat: fix shift params when calling run.bat

--- all.bat
+++ all.bat
@@ -5,18 +5,22 @@ for %%i in ("%~dp0.") do SET "script_dir=%%~fi"
 cd "%script_dir%"
 for %%i in ("%~dp0.") do SET "dirname=%%~ni"
 
+set "_args=%*"
 if "%1" == "rel" ( 
     set "barg=rel"
     shift
+    rem https://stackoverflow.com/questions/9363080/how-to-make-shift-work-with-in-batch-files
+    set "_args=!_args:*%1 =!"
 )
 
 if "%1" == "amd" ( 
     set "barg=%barg% amd"
     shift
+    set "_args=!_args:*%1 =!"
 )
 call build.bat %barg%
 if errorlevel 1 (
     echo ERROR BUILD 1>&2
     exit /b 1
 )
-call run.bat %*
+call run.bat %_args%
