8b026807724abcae0d1ea003204f5e51ed42fa91
VonC <vonc@laposte.net>
2022-04-21 15:39:34 +0200

    internal/print/print.go: add goroutine ID to Printf

--- internal/print/print.go
+++ internal/print/print.go
@@ -4,9 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"runtime"
+	"strconv"
+	"strings"
 )
 
 func Printf(msg string) {
+	msg = fmt.Sprintf("[%d] %s", goid(), msg)
 	// https://github.com/golang/go/issues/36619
 	bufStdout := bufio.NewWriter(os.Stdout)
 	_, err := bufStdout.WriteString(msg)
@@ -16,4 +20,17 @@ func Printf(msg string) {
 	} else {
 		fmt.Printf("Error on Printf '%s': %+v\n", msg, err)
 	}
+}
+
+// https://gist.github.com/metafeather/3615b23097836bc36579100dac376906
+// https://blog.sgmansfield.com/2015/12/goroutine-ids/
+func goid() int {
+	var buf [64]byte
+	n := runtime.Stack(buf[:], false)
+	idField := strings.Fields(strings.TrimPrefix(string(buf[:n]), "goroutine "))[0]
+	id, err := strconv.Atoi(idField)
+	if err != nil {
+		panic(fmt.Sprintf("cannot get goroutine id: %v", err))
+	}
+	return id
 }
